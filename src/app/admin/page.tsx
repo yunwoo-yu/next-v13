66d77edf0fe2b5b146760a6335b0a22fa2d5e46e
Ycow <skypnal12@gmail.com>
2023-08-26 00:59:44 +0900

    feat: user, admin 페이지 추가

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,5 @@
+const AdminPage = () => {
+  return <div>관리자만 볼 수 있는 페이지 입니다.</div>;
+};
+
+export default AdminPage;
---
+++ src/app/user/page.tsx
@@ -0,0 +1,5 @@
+const UserPage = () => {
+  return <div>로그인 된 유저만 볼 수 있는 페이지 입니다.</div>;
+};
+
+export default UserPage;
