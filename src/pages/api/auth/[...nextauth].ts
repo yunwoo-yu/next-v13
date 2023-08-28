3f8c566c970854e19b06ea50284a146bab2c2b57
Ycow <skypnal12@gmail.com>
2023-08-29 01:48:06 +0900

    feat: next-auth Client 생성, eslint 규칙 추가

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,19 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
