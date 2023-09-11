f0575ea2a3ce706d2e1924edaa9d3fe9912ec964
YunCow <skypnal12@gmail.com>
2023-09-11 21:31:32 +0900

    fix: 주석 오탈자 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,17 +7,17 @@ export const middleware = async (req: NextRequest) => {
   const session = await getToken({ req, secret: process.env.JWT_SECRET });
   const pathname = req.nextUrl.pathname;
 
-  // 로그인 유저 접근 가능
+  // 로그인 유저만 접근 가능
   if (pathname.startsWith('/user') && !session) {
     return NextResponse.redirect(new URL('/auth/login', req.url));
   }
 
-  // 어드민 유저 접근 가능
+  // 어드민 유저만 접근 가능
   if (pathname.startsWith('/admin') && session?.role !== 'Admin') {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
-  // 로그인한 유저가 회원가인, 로그인 페이지 접근 X
+  // 로그인한 유저가 회원가입, 로그인 페이지 접근 X
   if (pathname.startsWith('/auth') && session) {
     return NextResponse.redirect(new URL('/', req.url));
   }
